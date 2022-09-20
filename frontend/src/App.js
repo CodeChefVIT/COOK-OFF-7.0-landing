00c5b2e9864d172ddc117e6ca40da19812a1b687
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-09-20 19:21:26 +0530

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,23 @@
-import { Route, Routes, useLocation } from "react-router-dom";
-import "./App.css";
-import { AnimatePresence } from "framer-motion";
-
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Timeline from "./pages/Timeline";
-import Sponsors from "./pages/Sponsors";
-import HowReg from "./pages/HowReg";
-import Contact from "./pages/Contact";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const location = useLocation();
-
   return (
     <div className="App">
-      <AnimatePresence exitBeforeEnter>
-        <Routes location={location} key={location.key}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about-event" element={<About />} />
-          <Route path="/timeline-prizes" element={<Timeline />} />
-          <Route path="/past-sponsors" element={<Sponsors />} />
-          <Route path="/how-to-register" element={<HowReg />} />
-          <Route path="/contact-us" element={<Contact />} />
-        </Routes>
-      </AnimatePresence>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import { BrowserRouter as Router } from "react-router-dom";
-
-import "./../src/assets/fonts/Mudclaw/MUDCLAW.ttf";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
