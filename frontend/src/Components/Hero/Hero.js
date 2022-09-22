aca290fd9f2bdd5141926aa7c88b98c2d6fbd48c
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-09-22 21:36:29 +0530

    fix: responsiveness

--- frontend/src/Components/Hero/Hero.js
+++ frontend/src/Components/Hero/Hero.js
@@ -32,14 +32,14 @@ const Landing = () => {
         <div className={styles.container}>
           <div className={styles.content_container}>
             <div className={styles.title_heading_container}>
-              <h1 className="translate-x-14 -translate-y-14">COOK OFF</h1>
-              <p className="translate-x-14 font-mono -translate-y-14">
+              <h1 className="-translate-y-14">COOK OFF</h1>
+              <p className="font-mono -translate-y-14">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
                 enim ad minim veniam
               </p>
             </div>
-            <div className="translate-x-14 -translate-y-14">
+            <div className="-translate-y-14">
               <button className={styles.hero_button}>Register Now!</button>
             </div>
 
