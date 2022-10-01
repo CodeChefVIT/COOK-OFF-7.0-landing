e36ef2399f6bf9fd67d2ebf3f21033d77a5c2942
souvik150 <souvikmukherjee150@gmail.com>
2022-10-01 23:16:37 +0530

    fix: adds speaker

--- frontend/src/Components/Cards/Cards.js
+++ frontend/src/Components/Cards/Cards.js
@@ -7,8 +7,8 @@ const Cards = (props) => {
   return (
     <div className="flex justify-center">
       <div className="w-[70vw]">
-        <div className="flex flex-col md:flex-row lg:flex-row justify-between mt-20">
-          <div className="bg-[#F4CED3] border-4 border-black rounded-xl md:max-w-xs lg:min-w-max mb-4 mr-8 h-[100%]">
+        <div className="flex flex-col md:flex-row lg:flex-row justify-between mt-20 items-center">
+          <div className="bg-[#F4CED3] border-4 border-black rounded-xl md:max-w-xs lg:min-w-max mb-4 md:mr-8 h-[100%] w-[16rem] md:w-[32rem]">
             <div className="flex flex-row justify-between items-center py-3 px-5">
               <p>TIME</p>
               <img src={Clock} alt="clock" className="w-6 rounded-full" />
@@ -18,7 +18,7 @@ const Cards = (props) => {
               {props.data.time}
             </h1>
           </div>
-          <div className="bg-[#F4CED3] border-4 border-black rounded-xl md:max-w-xs lg:max-w-lg h-[100%]">
+          <div className="bg-[#F4CED3] border-4 border-black rounded-xl md:max-w-xs lg:max-w-lg h-[100%] w-[16rem] md:w-[32rem]">
             <div className="flex flex-row justify-between items-center py-3 px-5">
               <p>INFORMATION</p>
               <img src={Info} alt="clock" className="w-6 rounded-full" />
